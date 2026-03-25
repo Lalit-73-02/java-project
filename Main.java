b7a6fe43949d06e1d4a3380ee9703d58c428c4f1
Lalit-73-02 <lalitsinghkanyal53@gmail.com>
2026-03-25 21:02:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.print("Good Morning ");
+            System.out.print("Welcome ");
+        }
+
+    }
+}
+
+// for (int i = 1; i <= 5; i++) {
+//     System.out.print("Good Morning Welcome ");
+// }
