b7dd75b6c2244aa8d93d47a5c99030c1d67f3720
Lalit-73-02 <lalitsinghkanyal53@gmail.com>
2026-04-15 22:12:32 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -2,10 +2,15 @@ public class Practice {
     public static void main(String[] args) {
       int arr[] = {1,2,3,4,5,6,7,8,9,10};
 
-      for(int i = 0; i<=arr.length; i++)
-{
-  
+      for(int i = 0; i<=arr.length; i++){
+        boolean isduplicate = false;
+        for(int j= 0; j<=arr.length;j++){
+          if(arr[i] == arr[j]){
+            
+        }
 }
+
+
 
       
       
