479e6a2e5222d99a99770acd01c6454c2527f453
Lalit-73-02 <lalitsinghkanyal53@gmail.com>
2026-04-15 22:09:00 +0530

    Update code: add Practice.java

---
+++ Practice.java
@@ -0,0 +1,115 @@
+public class Practice {
+    public static void main(String[] args) {
+      int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+      for(int i = 0; i<=arr.length; i++)
+{
+  
+}
+
+      
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    int num = 123456;   REVERCE
+//        int rev = 0;
+
+//        while(num != 0){
+//         int digit = num % 10;
+//         rev = rev * 10 + digit;
+//         num = num / 10;
+//        }
+//        System.out.println(rev);
+
+
+
+//    int num = 2005;     PALINDROM
+//     int orignal = num;
+//     int rev = 0;
+
+//     while(num != 0){
+//         int digit = num % 10;
+//         rev = rev  * 10 + digit;
+//         num = num / 10;
+//     }
+//     if (orignal == num){
+//         System.out.println("Palindrome");
+//     }
+//     else{
+//         System.out.println("Not a Palindrome");
+//     }
+
+
+
+
+
+    //   int num = 5421;                COUNT
+    //     int count = 0;
+
+    //     while(num != 0){
+    //         count ++;
+    //         num = num /10;
+    //     }
+    //     System.out.println(count);
+
+
+
+
+//   int num = 153;           ARMSTRONG
+//         int orignal = num;
+//         int sum = 0;
+
+//         while (num !=0 ) {
+//             int digit = num % 10;
+//             sum += digit * digit * digit;
+//             num = num /10;
+//         }
+//         if(orignal == num){
+//              System.out.println("Armstrong");
+//         }
+//         else{
+//             System.out.println("not Armstrong");
+//         }
+
+
+
+      // int n = 451654564;         max number
+      // int max = 0;
+
+      // while(n != 0){
+      //   int digit = n % 10;
+      //   if(digit > max)
+      //       max = digit;
+
+      //   n = n / 10;
+      // }
+      // System.out.print(max);
+
+
+
