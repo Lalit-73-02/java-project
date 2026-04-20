06f4525d213e00327591113df8df26f5a28d025a
Lalit-73-02 <lalitsinghkanyal53@gmail.com>
2026-04-20 09:37:45 +0530

    Add files via upload

---
+++ FileChoos.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.filechooser.*;
+
+
+public class FileChoos {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Asus\\Downloads\\image.jpg");
+        frame.setIconImage(icon);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+
+
+
+
+        // FIle show
+        // JFileChooser jf = new JFileChooser("c:");
+        // jf.showSaveDialog(null);
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;;
+
 public class Main {
+
     public static void main(String[] args) {
+        JFrame f = new JFrame("Main");
+        f.setSize(400,400);
+        f.setVisible(true);
 
-        while (true) {
-            System.out.print("Good Morning ");
-            System.out.print("Welcome ");
-        }
-
+        DefaultMutableTreeNode style= new DefaultMutableTreeNode("style");
+        DefaultMutableTreeNode color= new DefaultMutableTreeNode("color");
+        DefaultMutableTreeNode font= new DefaultMutableTreeNode("font");
+        
+        style.add(color);
+        style.add(font);
+        DefaultMutableTreeNode red= new DefaultMutableTreeNode("Red");
+        DefaultMutableTreeNode green= new DefaultMutableTreeNode("Green");
+        DefaultMutableTreeNode blue= new DefaultMutableTreeNode("Blue");
+        color.add(red);
+        color.add(green);
+        color.add(blue);
+        JTree t= new JTree(style);
+        f.add(t);
+       
     }
 }
-
-// for (int i = 1; i <= 5; i++) {
-//     System.out.print("Good Morning Welcome ");
-// }
